0a9ddf227d24eef5f61a29c820ce24f982feca0c
Chinmay <kulkarnichinmay65@gmail.com>
2021-05-22 16:03:48 +0530

    First PHP file

---
+++ PHP/SyntaxandComments.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+Hello world
+<?php
+    echo"this is written by php";
+    /*Here <?php ..... ?> is the syntax of php. Here php is used in an html website which hides its source code and just prints what ever is instructed*/
+?>
+<?php
+    //Comments can either be entered by giving two forward slash or slash star just like above
+?>
+</body>
+</html>
