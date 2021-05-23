01c1f3e4db7ee5b294c8b39a1ca6cd079cef9f03
Chinmay <kulkarnichinmay65@gmail.com>
2021-05-23 13:10:25 +0530

    Variables & Data Types

---
+++ Python/Operators.py
@@ -0,0 +1,32 @@
+a = 3
+b = 5
+#Arithmetic Operator
+print("The value of a + b = ", a + b)
+print("The value of a - b = ", a - b)
+print("The value of a * b = ", a * b)
+print("The value of a / b = ", a / b)
+
+#Assignment Operator
+c = 3 # '=' is called assignemnt operator
+print(c)
+c += 3 # Here 3 will be added and then c will be printed
+print(c)
+c -= 3
+print(c)
+c *= 3
+print(c)
+c /= 3
+print(c)
+
+#Comparison Operator
+print(a==b) #this will return a boolean value
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a!=b) #This is not equal operator
+
+#Logical Operator
+print((a>b) and (a<b)) #this will give true if both have value true
+print((a>b) or (a<b)) #this will give true if either of one is true
+print(not(a==b)) #this will give opposite value of original 
--- Python/VariablesandDataTypes.py
+++ Python/VariablesandDataTypes.py
@@ -1,6 +1,40 @@
+#Variable name should not start with number
 a = "Doube" #Here it automatically identifies which type of data is stored
 b = 4.654 #most important comments are written by using hashtag
 c = 15 #these values are stored in the memory and these values are stored in containers which are variables
 d = 'Single'
 e = '''Triple'''
 #strings can be stored in three ways, as mentioned above.
+#Variable names are case sensitive
+# for tripple quote the string can be of multiple lines
+
+f = '''hello
+this 
+is 
+multi-line
+'''
+
+g = True #this is boolean which gives true or false
+
+h = None#this signifies none which has no value
+
+#printing Variables:
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+#each variable has its class which denotes its type. It can be given by print(type(variable_name))
+#printing the types of variable:
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
