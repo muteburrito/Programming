41249bc901d3ba58ce3a0e4c878726939999409d
Chinmay <kulkarnichinmay65@gmail.com>
2021-05-23 12:07:51 +0530

    2py

---
+++ Python/Variablesand DataTypes.py
@@ -0,0 +1,6 @@
+a = "Doube" #Here it automatically identifies which type of data is stored
+b = 4.654 #most important comments are written by using hashtag
+c = 15 #these values are stored in the memory and these values are stored in containers which are variables
+d = 'Single'
+e = '''Triple'''
+#strings can be stored in three ways, as mentioned above.
