f5173a33c295ecb7f235b282522222d03fc151c0
Chinmay <kulkarnichinmay65@gmail.com>
2021-05-25 20:54:40 +0530

    Operators and Variable

--- PHP/VariablesandOperators.php
+++ PHP/VariablesandOperators.php
@@ -82,6 +82,18 @@
 
     //Logical Operator
     echo "<h1>Logical Operator</h1>";
+    // there is AND (&&), OR (||), XOR, NOT(!)
+    $log = (true and true); // when both are true then only it will give true ow it will be false
+    echo var_dump($log);
+    echo "<br>";
+    $log = (false or true); //Even if one condtion is true it will be true
+    echo var_dump($log);
+    echo "<br>";
+    $log = (false xor true); //This condition requies true as well as flase to return true. if not then it will give false
+    echo var_dump($log);
+    echo "<br>";
+    $log = (!false); //This will invert the original value 
+    echo var_dump($log);
     echo "<br>";
 
 
@@ -89,16 +101,16 @@
     $Ivar = 0;
     echo "<h1>Increment Operator</h1>";
     echo "<br>";
-    $Ivar++; //This will +1 and print
+    echo $Ivar++; //This will first print the value and then add 1 to it and store it
     echo $Ivar;
     echo "<br>";
-    $Ivar--; // This will -1 and print
+    echo $Ivar--; // This fir print the value of va1 and then sub 1 and store it 
     echo $Ivar;
     echo "<br>";
-    ++$Ivar; 
+    echo ++$Ivar; // This will first add 1 to the value and then print it 
     echo $Ivar;
     echo "<br>";
-    --$Ivar;
+    echo --$Ivar; //Similarly this will sub 1 from the value and then print it
     echo $Ivar;
     echo "<br>"
     ?>
