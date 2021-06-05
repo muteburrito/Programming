91bf469fe3316c6c33f202ff708f79e97d248e43
Chinmay <kulkarnichinmay65@gmail.com>
2021-06-05 15:20:52 +0530

    Python

--- Python/VariablesandDataTypes.py
+++ Python/VariablesandDataTypes.py
@@ -1,11 +1,11 @@
-#Variable name should not start with number
-a = "Doube" #Here it automatically identifies which type of data is stored
-b = 4.654 #most important comments are written by using hashtag
-c = 15 #these values are stored in the memory and these values are stored in containers which are variables
+# Variable name should not start with number
+a = "Doube"  # Here it automatically identifies which type of data is stored
+b = 4.654  # most important comments are written by using hashtag
+c = 15  # these values are stored in the memory and these values are stored in containers which are variables
 d = 'Single'
 e = '''Triple'''
-#strings can be stored in three ways, as mentioned above.
-#Variable names are case sensitive
+# strings can be stored in three ways, as mentioned above.
+# Variable names are case sensitive
 # for tripple quote the string can be of multiple lines
 
 f = '''hello
@@ -14,11 +14,11 @@ is
 multi-line
 '''
 
-g = True #this is boolean which gives true or false
+g = True  # this is boolean which gives true or false
 
-h = None#this signifies none which has no value
+h = None  # this signifies none which has no value
 
-#printing Variables:
+# printing Variables:
 print(a)
 print(b)
 print(c)
@@ -28,8 +28,8 @@ print(f)
 print(g)
 print(h)
 
-#each variable has its class which denotes its type. It can be given by print(type(variable_name))
-#printing the types of variable:
+# each variable has its class which denotes its type. It can be given by print(type(variable_name))
+# printing the types of variable:
 print(type(a))
 print(type(b))
 print(type(c))
@@ -38,3 +38,12 @@ print(type(e))
 print(type(f))
 print(type(g))
 print(type(h))
+
+# We can also change the value a data type-
+
+z = '45'
+print(z)
+# total = z + 1 this will give error so we can concatinate this string to int
+z = int(z)
+total = z + 1
+print(total)
