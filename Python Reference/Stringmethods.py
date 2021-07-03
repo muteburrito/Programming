496f0ec690a4cf6db01580b694f67d38d9b26a87
Chinmay Kulkarni <chinmay.kulkarni@ubisoft.com>
2021-07-03 15:47:07 +0530

    py

---
+++ Python Reference/Stringmethods.py
@@ -0,0 +1,18 @@
+# We have many functions for strings
+
+a = 'Hello World'
+print(len(a))  # This method will return the length of that string
+# To view more functions we can write the variable name followed by a dot
+print(a.upper())  # Every function must be followed with a paranthesis
+
+# The above function will print the string in a upper case format
+# This method does not change the original variable
+
+print(a)
+print(a.lower())
+# We can use the find method to find a character in the string, this method will return the the index of that character
+print(a.find('W'))  # Anything we pass in the find method must be in quotes
+
+# We also have a function to replace:
+print(a.replace('World', 'Universe'))
+# Most of the methods are case sensitive.
