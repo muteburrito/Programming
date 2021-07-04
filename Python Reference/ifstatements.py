ddb9a53bb4c500fb9aad1d1d7a983cc0761240c7
Chinmay Kulkarni <chinmay.kulkarni@ubisoft.com>
2021-07-04 21:16:00 +0530

    py

---
+++ Python Reference/ifstatements.py
@@ -0,0 +1,13 @@
+is_hot = True
+is_cold = False
+
+if is_hot:
+    # In python indentation matters a lot. If not indented it will give error
+    print("Its a hot day")
+elif is_cold:
+    print("Its a cold day")
+else:
+    print("Its a lovely day")
+print("Have a good day")
+
+# Depending on the situation, it will print weather it is cold or hot. but the last line have a good day will be printed everytime
