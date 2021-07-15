679b8b7aa54bb6380c95bb73b89fdba56e7613d1
Chinmay Kulkarni <chinmay.kulkarni@ubisoft.com>
2021-07-15 08:59:56 +0530

    Py

--- Python Reference/Basics/WhileLoops.py
+++ Python Reference/Basics/WhileLoops.py
@@ -1,3 +1,6 @@
+# While loop has following syntax:
+# while 'condition': here the condition can be statement or a bollean value also
+# Block of code tp be executed
 ## i = 1
 # while i <= 5:  # Here we define the while loop that do the below steps while the condition is i < 5
 # print(i)
