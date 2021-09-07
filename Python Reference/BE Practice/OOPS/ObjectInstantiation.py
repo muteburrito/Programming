036f1023636f722a756b2ffe7d918b9d119f5413
Chinmay Kulkarni <kulkarnichinmay65@gmail.com>
2021-09-07 12:26:10 +0530

    Adding Python Only and Deleting other stuff from the repo

--- Python Reference/BE Practice/OOPS/ObjectInstantiation.py
+++ Python Reference/BE Practice/OOPS/ObjectInstantiation.py
@@ -4,7 +4,7 @@ class Form():
 
 # object is nothing but a variable:
 
-user1_form = Form()
+user1_form = Form() # Here an object user1_form is instanciated/created which uses data from class Form
 user1_form.name = 'CK'
 user1_form.address = 'Pune'
 user1_form.printdata()
