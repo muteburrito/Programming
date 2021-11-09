64d7d051e94f4a26c72e3a2471053a9031c63b84
ckulkarni <kulkarnichinmay65@gmail.com>
2021-11-09 17:21:59 +0530

    Update

---
+++ Java/Variables/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Printing my number:");
+
+        int myFirstNumber = 10;
+        System.out.print(myFirstNumber);
+    }
+}
