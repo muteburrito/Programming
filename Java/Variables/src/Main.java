eedf911e19cdd3d6a69e91427c8b51830b00bbf4
ckulkarni <kulkarnichinmay65@gmail.com>
2021-12-06 12:41:54 +0530

    [Remove] Deleting few folders to begin a fresh start

--- Java/Variables/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args){
-        System.out.println("Printing my number:");
-
-        int myFirstNumber = 10;
-        System.out.print(myFirstNumber);
-    }
-}
