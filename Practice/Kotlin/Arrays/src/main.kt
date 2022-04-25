66b0ff2dc661dcfc8f9fb116627600cdbf9bfaf7
ckulkarni <kulkarnichinmay65@gmail.com>
2022-04-25 12:38:11 +0530

    GitKraken

--- Practice/Kotlin/Arrays/src/main.kt
+++ Practice/Kotlin/Arrays/src/main.kt
@@ -27,4 +27,33 @@ fun main(){
     items.size
     println(items[1])
     // for list as well we can use a period and use different methods available
+
+    // This ia like a dictionary in python which has key value pair
+    val map = mapOf(1 to "A", 2 to "B", 3 to "C")
+
+    map.forEach{key, value -> println("$key -> $value")}
+
+    println()
+
+    // We cannot add or delete elements from collections by default in kotlin
+
+    // So to do so ->
+
+    val animals = mutableListOf("Dog", "Cat", "Hen")
+
+    println(animals)
+
+    animals.add(0,"Chicken")
+
+    println(animals)
+
+    val grades = mutableMapOf(1 to 'A', 2 to 'B', 3 to 'C')
+
+    println(grades)
+
+    grades.put(1, 'E')
+
+    println(grades)
+
+
 }
